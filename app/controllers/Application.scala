824439a0182c4034382f7db5a632ceb7cef06931
Ryan-Thomas <ryanthomas970@gmail.com>
2017-06-11 22:29:09 -0700

    Serving asynchronous results

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,16 +6,12 @@ import java.util.Date
 import play.api._
 import play.api.mvc._
 
+import scala.concurrent.Future
+
 class Application extends Controller {
-    // The action companion object is a DSL-like construct that helps define 
-    // controller methods.
-    def index = Action {
+    def index = Action.async {
         val date = new Date()
         val dateStr = new SimpleDateFormat().format(date)
-        // The index() method is a controller method which takes a compiled Scala 
-        // HTML template called index and returns it to the browser, also specifying
-        // 200 OK as a response code
-        Ok(views.html.index(dateStr))
-        // Basically, this just returns the index html template to the browser
+        Future.successful { Ok(views.html.index(dateStr)) }
     }
 }
