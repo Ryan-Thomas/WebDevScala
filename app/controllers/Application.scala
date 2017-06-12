d309eeef91b18044ced859174fcacfaa44370209
Ryan-Thomas <ryanthomas970@gmail.com>
2017-06-11 22:20:40 -0700

    Using template parameters

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,5 +1,8 @@
 package controllers
 
+import java.text.SimpleDateFormat
+import java.util.Date
+
 import play.api._
 import play.api.mvc._
 
@@ -7,10 +10,12 @@ class Application extends Controller {
     // The action companion object is a DSL-like construct that helps define 
     // controller methods.
     def index = Action {
+        val date = new Date()
+        val dateStr = new SimpleDateFormat().format(date)
         // The index() method is a controller method which takes a compiled Scala 
         // HTML template called index and returns it to the browser, also specifying
         // 200 OK as a response code
-        Ok(views.html.index())
+        Ok(views.html.index(dateStr))
         // Basically, this just returns the index html template to the browser
     }
 }
