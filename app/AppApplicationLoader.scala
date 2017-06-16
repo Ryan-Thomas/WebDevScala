46bb58d37687f13e8ce7ddc27c41b11d00432457
Ryan Thomas <rthomas@adaptivebiotech.com>
2017-06-16 12:57:41 -0700

    Managing application lifecycle

--- app/AppApplicationLoader.scala
+++ app/AppApplicationLoader.scala
@@ -5,6 +5,8 @@ import play.api.libs.ws.ahc.AhcWSComponents
 import play.api.routing.Router.Routes
 import services.{SunService, WeatherService}
 
+import scala.concurrent.Future
+
 class AppApplicationLoader extends ApplicationLoader {
     override def load(context: ApplicationLoader.Context): Application = {
         LoggerConfigurator(context.environment.classLoader).foreach { configurator =>
@@ -21,5 +23,13 @@ class AppApplicationLoader extends ApplicationLoader {
         // wsClient is defined in the AhcWSComponents trait
         lazy val sunService: SunService = wire[SunService]
         lazy val weatherService: WeatherService = wire[WeatherService]
+        applicationLifecycle.addStopHook { () => 
+            Logger.info("The app is about to stop")
+            Future.successful(Unit)
+        }
+        
+        val onStart = {
+            Logger.info("The app is about to start")
+        }
     }
 }
